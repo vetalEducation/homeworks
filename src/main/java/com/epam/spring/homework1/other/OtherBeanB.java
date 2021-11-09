21e023521eeef03f460699ae21a59839fdb0101a
Vetal Education <tyu96@ukr.net>
2021-11-09 14:23:19 +0200

    Refactor OtherBeanB

--- src/main/java/com/epam/spring/homework1/other/OtherBeanB.java
+++ src/main/java/com/epam/spring/homework1/other/OtherBeanB.java
@@ -1,6 +1,7 @@
 package com.epam.spring.homework1.other;
 
 import com.epam.spring.homework1.beans.BeanB;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,6 +9,7 @@ public class OtherBeanB {
 
     private BeanB beanB;
 
+    @Autowired
     public void setBeanB(BeanB beanB) {
         System.out.println(this.getClass().getSimpleName() + ". " + beanB.getClass().getSimpleName()
                 + " was injected through the setter");

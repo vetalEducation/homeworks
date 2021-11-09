591898720a1be99e5954b52e9bf6c30e2324395e
Vetal Education <tyu96@ukr.net>
2021-11-09 14:22:11 +0200

    Add injection of BeanB for OtherBeanB via the setter

--- src/main/java/com/epam/spring/homework1/other/OtherBeanB.java
+++ src/main/java/com/epam/spring/homework1/other/OtherBeanB.java
@@ -1,7 +1,16 @@
 package com.epam.spring.homework1.other;
 
+import com.epam.spring.homework1.beans.BeanB;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OtherBeanB {
+
+    private BeanB beanB;
+
+    public void setBeanB(BeanB beanB) {
+        System.out.println(this.getClass().getSimpleName() + ". " + beanB.getClass().getSimpleName()
+                + " was injected through the setter");
+    }
+
 }

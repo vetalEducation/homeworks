8df28e2b47c4152927c0745d7990987d9673000f
Vetal Education <tyu96@ukr.net>
2021-11-09 14:20:36 +0200

    Add injection of BeanA for OtherBeanA via the constructor

--- src/main/java/com/epam/spring/homework1/other/OtherBeanA.java
+++ src/main/java/com/epam/spring/homework1/other/OtherBeanA.java
@@ -1,7 +1,16 @@
 package com.epam.spring.homework1.other;
 
+import com.epam.spring.homework1.beans.BeanA;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OtherBeanA {
+
+    private BeanA beanA;
+
+    public OtherBeanA(BeanA beanA) {
+        System.out.println(this.getClass().getSimpleName() + ". " + beanA.getClass().getSimpleName()
+        + " was injected through the constructor");
+    }
+
 }

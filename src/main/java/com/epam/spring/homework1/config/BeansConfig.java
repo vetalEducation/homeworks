11915ae289e6ec691abb14b5bd0904c847c615e1
Vetal Education <tyu96@ukr.net>
2021-11-09 14:29:21 +0200

    Add component scan for BeansConfig

--- src/main/java/com/epam/spring/homework1/config/BeansConfig.java
+++ src/main/java/com/epam/spring/homework1/config/BeansConfig.java
@@ -1,7 +1,9 @@
 package com.epam.spring.homework1.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan("com.epam.spring.homework1.beans")
 public class BeansConfig {
 }

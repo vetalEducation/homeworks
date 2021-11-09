20378946854300a24ac91f3b18cb1f5d45558a22
Vetal Education <tyu96@ukr.net>
2021-11-09 15:54:45 +0200

    Add beans Animal, Dog, Car, Cheetah, Spider to PetConfig

--- src/main/java/com/epam/spring/homework1/config/PetConfig.java
+++ src/main/java/com/epam/spring/homework1/config/PetConfig.java
@@ -1,9 +1,35 @@
 package com.epam.spring.homework1.config;
 
+import com.epam.spring.homework1.pet.*;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan("com.epam.spring.homework1.pet")
+@ComponentScan(includeFilters = @ComponentScan.Filter(
+        type = FilterType.ASSIGNABLE_TYPE, value = Spider.class))
 public class PetConfig {
+
+    @Bean
+    public Animal dog() {
+        return new Dog();
+    }
+
+    @Bean
+    public Animal cat() {
+        return new Cat();
+    }
+
+    @Bean
+    public Animal cheetah() {
+        return new Cheetah();
+    }
+
+    @Bean
+    public Animal spider() {
+        return new Spider();
+    }
+
 }
